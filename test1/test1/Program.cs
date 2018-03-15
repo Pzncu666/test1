97bc769a63cfe63893ee8be0d5fd716d2437ebab
m305-38\w401 <ddcc21002gmail.com>
2018-03-15 18:01:06 +0800

    01

---
+++ test1/test1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+
+            Console.WriteLine("╔════════╗" + "\r\n" + "║      DMA       ║" + "\r\n" + "╚════════╝");
+
+            double rate = 0.2771;
+
+            Console.WriteLine("請輸入日幣:");
+
+            string input = Console.ReadLine();
+
+            double total = int.Parse(input) * rate;
+
+            Console.WriteLine("換算為台幣:{0}", total);
+
+            Console.ReadLine();
+        }
+    }
+}
